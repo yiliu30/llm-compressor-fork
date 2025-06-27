160341759edb462e503e9e950d28bab9c54e6ce9
yiliu30 <yi4.liu@intel.com>
2025-06-27 06:45:50 -0400

    format code
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- src/llmcompressor/modifiers/quantization/calibration.py
+++ src/llmcompressor/modifiers/quantization/calibration.py
@@ -12,12 +12,13 @@ from compressed_tensors.quantization.lifecycle.forward import forward_quantize
 from compressed_tensors.quantization.utils import (
     is_fp4,
     is_kv_cache_quant_scheme,
+    is_mx,
     is_mxfp4,
 )
 from compressed_tensors.utils import align_module_device, update_parameter_data
 from loguru import logger
 from torch.nn import Module
-from compressed_tensors.quantization.utils import is_fp4, is_kv_cache_quant_scheme, is_mxfp4, is_mx
+
 from llmcompressor.modifiers.quantization.cache import QuantizedKVParameterCache
 from llmcompressor.observers import Observer
 from llmcompressor.utils.helpers import getattr_chain
